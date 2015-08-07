f089321ca556cbf0204d67a471da7232afe04f01
George Dean and Neha Batra <pair+gdean+nbatra@pivotal.io>
2015-08-07 12:26:46 -0700

    Remove unnecessary @Component on ActivityRepository

--- activities/src/main/java/io/pivotal/activities/repositories/ActivityRepository.java
+++ activities/src/main/java/io/pivotal/activities/repositories/ActivityRepository.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import io.pivotal.activities.models.Activity;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface ActivityRepository extends CrudRepository<Activity, Long> {
     List<Activity> findByDestinationId(Long destinationId);
 }
